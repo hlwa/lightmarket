d0a277c3d183deb3e1893d9feaab447c8fc3583d
Georgina Bada <georginabada@gmail.com>
2022-01-09 21:49:22 -0500

    Adding user table and product table with seed data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,10 +1,9 @@
-INSERT INTO products
-  (id,name, seller_id, price, sold, description)
-VALUES(
-  1, ‘Travel Size Purell’, 1, ‘2.99’, False, '
-  - Purell Instant Hand Sanitizer kills 99.99% of germs with a refreshing gel’
-),
-(2,‘Lysol Handi-pack Disinfecting Wipes’,1, ‘14.99’, False, '
-- 80 Count (Pack of 4
-- Lysol Disinfecting Wipes can be used as a convenient way to clean and disinfect your household surfaces’
- ),
+
+INSERT INTO users
+  (is_seller,username,email, mobile, password)
+VALUES
+  (True, ‘Georgina’, ‘georgina@email.com’, 555-555-5551, ‘1234’, ‘nigeria’, ‘Lagos’, ‘Lagos’, ‘First street’,‘M4532’),
+  (‘Hailan’,‘hailan@email.com’, 555-555-5552, ‘abcd’, ‘Canada’, ‘ontario’, ‘toronto’, ‘2nd steet’,‘W1W 7R7’),
+  (‘Duygu’,‘duygu@email.com’, 555-555-5553 ‘5678’, ‘Canada’,‘Vancouver’, ‘whistler’,‘Park st’,‘V5K 0A5’),
+  (‘Bob’, ‘bob@email.com’,555-555-5554,‘efgh’, ‘Canada’, ‘Alberta’, ‘Fort McMurray’, ‘Main st’, ‘K1A 0B1’),
+  (‘Thomas’, ‘thomas@email.com’, 555-555-5555, ‘4321’, ‘Canada’, ‘Nova Scotia’,‘Halifax’,‘Water St’ ,‘B3H 0A2’ );
---
+++ db/seeds/02_products.sql
@@ -0,0 +1,57 @@
+INSERT INTO products
+  (id,name, seller_id, price, sold, description)
+VALUES(
+  1, ‘Travel Size Purell’, 1, ‘2.99’, False, '
+  - Purell Instant Hand Sanitizer kills 99.99% of germs with a refreshing gel’
+),
+(2,‘Lysol Handi-pack Disinfecting Wipes’,1, ‘14.99’, False, '
+- 80 Count (Pack of 4
+- Lysol Disinfecting Wipes can be used as a convenient way to clean and disinfect your household surfaces’
+ ),
+
+(3, ‘RAPID COVID-19 ANTIGEN TEST KIT (5-PACK)’, 1, ‘80.00’, '
+5-Pack of Rapid COVID-19 Antigen Tests (Nasal or Nasopharyngeal swabs)
+Each Rapid Test Kit Contains:
+- 5 x Test cassettes
+- 5 x Individually packed swabs
+- 5 x individually packed buffers
+- 5 x tubes and nozzles
+- 1 x Tube stand
+- 1 x Product insert
+'
+),
+
+(4, ‘Disposable Face Mask (50pcs)’, 1, ‘12.99’, '
+- Sold online exclusively.
+- Full width adjustable nose strip.
+- Outer Layer: 100% Spunbond Polypropylene; Central Layer: 100% Melt Blown Polypropylene; Inner Layer: 100% Spunbond Polypropylene
+- Quality control, inspected and shipped from  Canada
+- This is Non-Medical Mask
+- Does not contain Graphene’ ),
+(5, ‘Finger Oxi-Meter Monitor for Adults and Children’, 1, ‘28.79’, false,  '
+- CE Certificated (directly authorized by EC-REP).
+- FDA (510k) premarket submission approval.
+- 8s automatic shutdown, energy saving.
+- One-button operation, simple and convenient.
+- Small size, portable, lightweight, easy to carry.
+- 4-directional screen display with 6 display mode.
+- Infrared detection, fast measurement in 5 seconds.
+- SPO2 + heart rate sensor - AMS chips imported from Austria.
+- Free lanyard included for carrying.
+- Powered by 2 AAA batteries (not included.
+' ),
+(6,‘Tylenol Pain Reliever/Fever Reducer, Extra Strength, 500 Mg’, 1, ‘39.70’, false,
+'- Temporarily relieves minor aches and pains due to: the common cold; headache; backache; minor pain of arthritis, toothache; muscular aches; premenstrual and menstrual cramps
+- Contains acetaminophen. Contains no aspirin. 50 pouches of 2 caplets each. This package for households without young children
+- Temporarily reduces fever'
+),
+(7, ‘Non-Contact Infrared Forehead Thermometer’, 1, ‘29.99’, false, '
+- This item does not include batteries.
+- Non Contact and 1 Second Measurement: One-button fast temperature measurement. Non-contact infrared measurement, while avoiding cross-infection. Measuring distance is 1-3cm.
+' ),
+(8, ‘10 Pack Transparent Face Shield Suitable’, 1, ‘31.99’, false ,'
+- CLEAR FACE SHIELD: Lightweight & easy to see through anti-fog face cover helps protect you from illness-causing , while the wrap-around design is great for 180-degree full face protection without any gaps for increased safety.
+- BULK PROTECTION: Includes ten (10) reusable full face shield masks, and is perfect for men, women, and children
+- COMFORTABLE DESIGN: Our face shields feature a foam headband that relieves pressure while the large shield allows room to wear glasses.
+- ONE SIZE FITS ALL: Our protective plastic face shield with adjustable elastic headband fits all sizes.
+’);
