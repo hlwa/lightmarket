a664fd8c4d24d18b7c5b68e648215b4b9b37317a
Georgina Bada <georginabada@gmail.com>
2022-01-09 19:13:16 -0500

    test

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,10 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO products
+  (id,name, seller_id, price, sold, description)
+VALUES(
+  1, ‘Travel Size Purell’, 1, ‘2.99’, False, '
+  - Purell Instant Hand Sanitizer kills 99.99% of germs with a refreshing gel’
+),
+(2,‘Lysol Handi-pack Disinfecting Wipes’,1, ‘14.99’, False, '
+- 80 Count (Pack of 4
+- Lysol Disinfecting Wipes can be used as a convenient way to clean and disinfect your household surfaces’
+ ),
