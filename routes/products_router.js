cb00983909b4defa2515def5e88ba5cb7ade5d3b
Hailan Wang <hailan6257@email.com>
2022-01-10 22:31:18 -0500

    update router

--- routes/products_router.js
+++ routes/products_router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const productQueries = require('../db/product-queries');
+const productQueries = require('../db/queries/products_queries');
 
 //router for home_page, admin_center
 // GET /products/
