2bd038566126f02c42c0ea0943377ef61e5be3fd
DuyguY <duygu.yldrm4820@gmail.com>
2022-01-13 11:32:15 -0500

    update sql

--- db/seeds/02_products.sql
+++ db/seeds/02_products.sql
@@ -2,7 +2,7 @@ INSERT INTO products
   (id,name, seller_id, price, sold, description, url)
 VALUES(
   1, 'Travel Size Purell', 1, '2.99', False, '
-  - Purell Instant Hand Sanitizer kills 99.99% of germs with a refreshing gel', "'png/sanitizer.jpg'"
+  - Purell Instant Hand Sanitizer kills 99.99% of germs with a refreshing gel', 'https://www.pngall.com/wp-content/uploads/5/Hand-Sanitizer-PNG-Free-Download.png'
 ),
 
 
@@ -11,7 +11,7 @@ VALUES(
 - Kills 99.99% of viruses & bacteria
 - Kills over 100 illness causing germs, including cold & flu viruses
 - Kills 99.9% of odor causing bacteria
-- Kills over 100 illness causing germs including the virus that causes covid19 (sars-cov-2) on-hard non-porus surfaces', '/png/lysol.jpg'
+- Kills over 100 illness causing germs including the virus that causes covid19 (sars-cov-2) on-hard non-porus surfaces', 'https://cdn.shopify.com/s/files/1/0288/6949/2811/products/Lysol_Lingettes_desinfectantes_30_StopGerms.jpg?v=1608588965'
  ),
 
 (3, 'RAPID COVID-19 ANTIGEN TEST KIT (5-PACK)', 1, '80.00',False,  '
@@ -22,7 +22,7 @@ Each Rapid Test Kit Contains:
 - 5 x individually packed buffers
 - 5 x tubes and nozzles
 - 1 x Tube stand
-- 1 x Product insert', '/png/rapidtest.jpg'
+- 1 x Product insert', 'https://www.mygrandeprairienow.com/wp-content/uploads/2021/05/Rapid-test-2.jpg'
 
 ),
 
@@ -32,7 +32,7 @@ Each Rapid Test Kit Contains:
 - Outer Layer: 100% Spunbond Polypropylene; Central Layer: 100% Melt Blown Polypropylene; Inner Layer: 100% Spunbond Polypropylene
 - Quality control, inspected and shipped from  Canada
 - This is Non-Medical Mask
-- Does not contain Graphene' ,'png/facemask.jpeg'),
+- Does not contain Graphene' ,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR83jRjkYYIA0Drt6hwajJdjeaZk_Zv0wELeA&usqp=CAU'),
 
 (5, 'Finger Oxi-Meter Monitor for Adults and Children', 1, '28.79', false,  '
 - CE Certificated (directly authorized by EC-REP).
@@ -45,30 +45,30 @@ Each Rapid Test Kit Contains:
 - SPO2 + heart rate sensor - AMS chips imported from Austria.
 - Free lanyard included for carrying.
 - Powered by 2 AAA batteries (not included.
-' ,'png/oximeter.jpg'),
+' ,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkGB38P9mIYH7-1cs8DC1-cZWeFnQBqab_M02BhC5edhHpM9RCekcsWFUzV2vXkTZdrVY&usqp=CAU'),
 
 (6,'Tylenol Pain Reliever/Fever Reducer, Extra Strength, 500 Mg', 1, '39.70', false,
 '- Temporarily relieves minor aches and pains due to: the common cold; headache; backache; minor pain of arthritis, toothache; muscular aches; premenstrual and menstrual cramps
 - Contains acetaminophen. Contains no aspirin. 50 pouches of 2 caplets each. This package for households without young children
-- Temporarily reduces fever', 'png/meds.jpg'
+- Temporarily reduces fever', 'https://www.tylenol.com/sites/tylenol_us/files/styles/product_image/public/product-images/tyl_8hr_arthritis_pain_charm_bty_ft.jpg'
 ),
 
 (7, 'Non-Contact Infrared Forehead Thermometer', 1, '29.99', false, '
 - This item does not include batteries.
 - Non Contact and 1 Second Measurement: One-button fast temperature measurement. Non-contact infrared measurement, while avoiding cross-infection. Measuring distance is 1-3cm.
-', 'png/thermometer.jpg' ),
+', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9B8M2dDI5yJlF7D44dRV3L_LcFn6D0BcMew&usqp=CAU' ),
 
 (8, '10 Pack Transparent Face Shield Suitable', 1, '31.99', false ,'
 - CLEAR FACE SHIELD: Lightweight & easy to see through anti-fog face cover helps protect you from illness-causing , while the wrap-around design is great for 180-degree full face protection without any gaps for increased safety.
 - BULK PROTECTION: Includes ten (10) reusable full face shield masks, and is perfect for men, women, and children
 - COMFORTABLE DESIGN: Our face shields feature a foam headband that relieves pressure while the large shield allows room to wear glasses.
 - ONE SIZE FITS ALL: Our protective plastic face shield with adjustable elastic headband fits all sizes.
-', 'png/faceshield.jpg'),
+', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNHjDhSMC2Dfsx-njQk_9yLPu7nSdV7wYpew&usqp=CAU'),
 
 (9, 'COVID-19 Stickers Pack', 1, '5.00', false, '
 - Pack includes 30 stickers.
 - Keep away from children.
-', 'png/sticker.jpg' )
+', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZojyp5GkyITqicSjQuKN1uwdEV_XB1Xx3Fg&usqp=CAU' )
 
 ;
 
